f8e61c88f56f5272781458464c38e3fdb2c6df42
Leonardo Gama <leogama09@gmail.com>
2023-01-12 11:18:12 -0300

    First component

---
+++ react_com_ts/src/components/FirstComponent.tsx
@@ -0,0 +1,12 @@
+// 3- Componente
+import React from 'react'
+
+function FirstComponent() {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default FirstComponent
