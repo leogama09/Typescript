be8a13fff979ee1608e18a2ecae075929886638e
Leonardo Gama <leogama09@gmail.com>
2023-01-12 10:50:25 -0300

    react & TS

---
+++ react_com_ts/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com TS</h1>
+    </div>
+  );
+}
+
+export default App;
