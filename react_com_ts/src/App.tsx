03e701d75d33638ea80173a3db7db99e80a97b79
Leonardo Gama <leogama09@gmail.com>
2023-01-12 12:24:14 -0300

    types

--- react_com_ts/src/App.tsx
+++ react_com_ts/src/App.tsx
@@ -10,6 +10,11 @@ import Destructuring, { Category } from './components/Destructuring';
 // 6- useState
 import State from './components/State';
 
+// 8- type
+type textOrNull = string | null
+
+type fixed = "Isso" | "Ou" | "Aquilo"
+
 function App() {
 
 // 1-Variáveis
@@ -22,6 +27,13 @@ const userGreeting = (name:string): string => {
   return `Olá, ${name}! `
 }
 
+// 8- type
+const myText: textOrNull = "Tem algum texto aqui"
+let mySecondText: textOrNull = null
+
+// mySecondText = "opa"
+
+const testandoFixed:fixed = "Isso"
 
   return (
     <div className="App">
@@ -44,6 +56,8 @@ const userGreeting = (name:string): string => {
         category={Category.P}
       />
       <State />
+      {myText && <p>Tem texto na variável</p>}
+      {mySecondText && <p>Tem texto na variável</p>}
     </div>
   );
 }
