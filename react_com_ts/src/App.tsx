f4fe925cb0a6c50118b45d841c3ee98c2b909f4c
Leonardo Gama <leogama09@gmail.com>
2023-01-12 11:06:49 -0300

    Variables

--- react_com_ts/src/App.tsx
+++ react_com_ts/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+// 1-Variáveis
+const name: string = "Leonardo"
+const age: number = 30
+const isWorking: boolean = true
+
+
   return (
     <div className="App">
       <h1>React com TS</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <p>Está trabalhando!</p>
+      )}
     </div>
   );
 }
